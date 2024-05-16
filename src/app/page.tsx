60959ef6ec6d66b829dfbdebf6f5332c3ff391ea
MthokoTheDeveloper <mthokodev@gmail.com>
2024-05-16 20:22:30 +0200

    Create Revenue, Subs, Sales and Activity cards on dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { CardProps } from "@/components/ui/Card";
+import Card, { CardProps } from "@/components/ui/Card";
 import PageTitle from "@/components/ui/PageTitle";
 import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
 
@@ -33,7 +33,13 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-5 w-full">
       <PageTitle title="Dashboard"/>
-      <section className="grid "></section>
+      <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all
+      sm:grid-cols-2 xl:grid-cols-4">
+        {cardData.map((d, i)=>
+        <Card key={i} amount={d.amount}
+            description={d.description} icon={d.icon} 
+            label={d.label} />)}
+      </section>
     </div>
   );
 }
